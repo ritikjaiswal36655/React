bd2101a42028ad249e27023c9f167aaff4c811ed
ritikjaiswal36655 <ritikjaiswal36655@gmail.com>
2025-03-14 13:31:26 +0530

    Initialize React project with Vite setup and basic counter functionality

---
+++ 01_Project_Counter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const increment = () => {
+    setCount(count+1)
+  } 
+  const decrease = () => {
+    if (count === 0) {
+      setCount(0)
+    } else {
+      setCount(count-1)
+    }
+    
+  }
+  return (
+    <>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment} >Increase</button>
+      <button onClick={decrease} >decrease</button>
+    </>
+  )
+}
+
+export default App
